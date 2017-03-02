e05f08d5beaf28312fe789069e7accae875a8f6d
Louis CarricoBraun <lcarrico-braun@wlapcs.org>
2017-03-02 20:19:05 +0000

    randomizers

--- sketch.js
+++ sketch.js
@@ -2,10 +2,19 @@ var a = 100;
 var b = 100;
 var newHeight = 50;
 var newWidth = 50;
+var ranRgb;
+var ranRgb2;
+var ranRgb3;
+var ranBack;
+var ranBack2;
+var ranBack3;
 
 function setup(){
+    ranBack = random(0, 255);
+    ranBack2 = random(0, 255);
+    ranBack3 = random(0, 255);
     createCanvas(600, 400);
-    background(163, 19, 198);
+    background(ranBack, ranBack2, ranBack3);
     
 
 }
@@ -14,8 +23,11 @@ function setup(){
 function draw(){
     //background(150);
     if(mouseIsPressed){
+        ranRgb = random(0, 255);
+        ranRgb2 = random(0, 255);
+        ranRgb3 = random(0, 255);
         noStroke();
-        fill(225, 255, 0, 100);
+        fill(ranRgb, ranRgb2, ranRgb3, 50);
         ellipse(mouseX, mouseY, newWidth, newHeight);
     }
     
